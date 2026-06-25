15df4050a1b5026a0f7308c7e64ff34a7be02c68
Kelme <kelme589@gmail.com>
2026-06-25 18:06:08 -0300

    Cria decap-cms-oauth-provider básico

---
+++ decap-cms-oauth-provider/server.js
@@ -0,0 +1,17 @@
+﻿const express = require("express");
+const bodyParser = require("body-parser");
+const fetch = require("node-fetch");
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get("/auth", (req, res) => {
+  res.send("OAuth Provider Auth Endpoint funcionando!");
+});
+
+app.get("/callback", (req, res) => {
+  res.send("OAuth Provider Callback Endpoint funcionando!");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Provider rodando na porta ${port}`));
